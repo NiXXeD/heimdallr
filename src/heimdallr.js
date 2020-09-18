bc4a11b30afdb2d7b6d6dcb56dfbfe7dd0d29a61
Andy Christianson <nix@nixxed.com>
2020-09-18 12:28:57 -0500

    refactor: Split main logic to separate file.

---
+++ src/heimdallr.js
@@ -0,0 +1,98 @@
+const chalk = require('chalk')
+const bitbucket = require('./bitbucket')
+const moment = require('moment')
+const inquirer = require('inquirer')
+const open = require('open')
+const {cache, updateCache, cleanCache} = require('./cache')
+
+module.exports = async ({project, baseUrl, token, email}) => {
+    const {values: repos} = await bitbucket.get(`projects/${project}/repos`)
+    const repoPRs = await Promise.all(
+        repos.map(async repo => {
+            const {values: PRs} = await bitbucket.get(`projects/${project}/repos/${repo.slug}/pull-requests`)
+
+            return Promise.all(PRs.map(async pr => {
+                const {values: activities} = await bitbucket.get(`projects/${project}/repos/${repo.slug}/pull-requests/${pr.id}/activities`)
+                return {
+                    ...pr,
+                    activities,
+                    repo
+                }
+            }))
+        })
+    )
+    const openPrIds = []
+    const choices = repoPRs
+        .flat()
+        .map(pr => {
+            const value = pr.links.self[0].href
+            openPrIds.push(value)
+            let message = ''
+            // PR#123    feat(something): Should do a thing
+            const prNumber = chalk.gray(`PR#${`${pr.id}`.padEnd(5)}`)
+            const prTitle = chalk.white(pr.title)
+            message += `${prNumber} ${prTitle}\n`
+
+            // some-repo-name    an.email@whatever.com    25 nanoseconds ago
+            const repoName = chalk.gray(pr.repo.name)
+            const author = chalk.gray(pr.author.user.emailAddress)
+            const updated = chalk.gray(moment(pr.updatedDate).fromNow())
+            const newActivities = pr.activities
+                .filter(activity => !cache[value] || activity.createdDate > cache[value])
+            const selfActivity = newActivities
+                .findIndex(activity => activity.user.emailAddress === email)
+            const newActivityCount = selfActivity >= 0
+                ? Math.min(selfActivity, newActivities.length)
+                : newActivities.length
+            const activityCountText = newActivityCount > 0 ? chalk.red(`+${newActivityCount}`.padEnd(7)) : '       '
+            message += `   ${activityCountText} ${repoName}\t ${author}\t${updated}\n`
+
+            // +123   an.approver@whatever.com, someone.else@whatever.com
+            const reviewers = pr.reviewers.length
+                ? pr.reviewers.map(reviewer => {
+                    const color = {
+                        UNAPPROVED: chalk.yellow,
+                        APPROVED: chalk.green,
+                        NEEDS_WORK: chalk.red
+                    }[reviewer.status]
+                    return color(reviewer.user.emailAddress)
+                }).join(chalk.white(', '))
+                : ''
+
+            const totalActivity = chalk.yellow(`${pr.activities.length}`.padEnd(6))
+            message += `    ${totalActivity} ${reviewers}\n`
+
+            return {name: message, value}
+        })
+
+    // Clean out PRs that are no longer open from the cache
+    cleanCache(openPrIds)
+
+    console.clear()
+    prompt = inquirer.prompt([
+        {
+            type: 'list',
+            pageSize: 20,
+            name: 'choice',
+            message: `All open PRs currently in BitBucket Project ${project}`,
+            choices: [
+                new inquirer.Separator(),
+                {
+                    name: chalk.green('Refresh data ') + chalk.bold(`(last refreshed at ${moment().format('h:mm:ss a')})`),
+                    value: 'refresh'
+                },
+                new inquirer.Separator(),
+                ...choices
+            ]
+        }
+    ])
+    const {choice} = await prompt
+
+    if (choice && choice !== 'refresh') {
+        // Update cache with new value
+        updateCache(choice)
+
+        // Open selected PR in browser
+        open(choice)
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 #!/usr/bin/env node
 
-const bitbucket = require('./bitbucket')
 const chalk = require('chalk')
-const moment = require('moment')
-const inquirer = require('inquirer')
-const open = require('open')
-const {cache, updateCache, cleanCache} = require('./cache')
+const heimdallr = require('./heimdallr')
 
 const {
     HEIMDALLR_PROJECT: project,
@@ -25,108 +21,18 @@ const restartTimer = () => {
     clearInterval(timerId)
     timerId = setTimeout(() => {
         prompt.ui.close()
-        return doStuff()
+        return main()
     }, 5 * 60 * 60 * 1000)
 }
 
-const doStuff = async () => {
+const main = async () => {
     try {
         restartTimer()
-
-        const {values: repos} = await bitbucket.get(`projects/${project}/repos`)
-        const repoPRs = await Promise.all(
-            repos.map(async repo => {
-                const {values: PRs} = await bitbucket.get(`projects/${project}/repos/${repo.slug}/pull-requests`)
-
-                return Promise.all(PRs.map(async pr => {
-                    const {values: activities} = await bitbucket.get(`projects/${project}/repos/${repo.slug}/pull-requests/${pr.id}/activities`)
-                    return {
-                        ...pr,
-                        activities,
-                        repo
-                    }
-                }))
-            })
-        )
-        const openPrIds = []
-        const choices = repoPRs
-            .flat()
-            .map(pr => {
-                const value = pr.links.self[0].href
-                openPrIds.push(value)
-                let message = ''
-                // PR#123    feat(something): Should do a thing
-                const prNumber = chalk.gray(`PR#${`${pr.id}`.padEnd(5)}`)
-                const prTitle = chalk.white(pr.title)
-                message += `${prNumber} ${prTitle}\n`
-
-                // some-repo-name    an.email@whatever.com    25 nanoseconds ago
-                const repoName = chalk.gray(pr.repo.name)
-                const author = chalk.gray(pr.author.user.emailAddress)
-                const updated = chalk.gray(moment(pr.updatedDate).fromNow())
-                const newActivities = pr.activities
-                    .filter(activity => !cache[value] || activity.createdDate > cache[value])
-                const selfActivity = newActivities
-                    .findIndex(activity => activity.user.emailAddress === email)
-                const newActivityCount = selfActivity >= 0
-                    ? Math.min(selfActivity, newActivities.length)
-                    : newActivities.length
-                const activityCountText = newActivityCount > 0 ? chalk.red(`+${newActivityCount}`.padEnd(7)) : '       '
-                message += `   ${activityCountText} ${repoName}\t ${author}\t${updated}\n`
-
-                // +123   an.approver@whatever.com, someone.else@whatever.com
-                const reviewers = pr.reviewers.length
-                    ? pr.reviewers.map(reviewer => {
-                        const color = {
-                            UNAPPROVED: chalk.yellow,
-                            APPROVED: chalk.green,
-                            NEEDS_WORK: chalk.red
-                        }[reviewer.status]
-                        return color(reviewer.user.emailAddress)
-                    }).join(chalk.white(', '))
-                    : ''
-
-                const totalActivity = chalk.yellow(`${pr.activities.length}`.padEnd(6))
-                message += `    ${totalActivity} ${reviewers}\n`
-
-                return {name: message, value}
-            })
-
-        // Clean out PRs that are no longer open from the cache
-        cleanCache(openPrIds)
-
-        console.clear()
-        prompt = inquirer.prompt([
-            {
-                type: 'list',
-                pageSize: 20,
-                name: 'choice',
-                message: `All open PRs currently in BitBucket Project ${project}`,
-                choices: [
-                    new inquirer.Separator(),
-                    {
-                        name: chalk.green('Refresh data ') + chalk.bold(`(last refreshed at ${moment().format('h:mm:ss a')})`),
-                        value: 'refresh'
-                    },
-                    new inquirer.Separator(),
-                    ...choices
-                ]
-            }
-        ])
-        const {choice} = await prompt
-
-        if (choice && choice !== 'refresh') {
-            // Update cache with new value
-            updateCache(choice)
-
-            // Open selected PR in browser
-            open(choice)
-        }
-
-        return doStuff()
+        await heimdallr({project, baseUrl, token, email})
+        return main()
     } catch (ex) {
         console.log(chalk.red('Something went wrong!'), ex)
         process.exit(1)
     }
 }
-return doStuff()
+return main()
